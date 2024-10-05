62ba621d78f6682529c1211c385a677ec147b4e9
Paul Oldenburger <55639625+unnamddd@users.noreply.github.com>
2024-10-05 23:13:32 +0200

    feat: added simple example

---
+++ examples/editor/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Editor from '../../../src/index'
+
+function App(): React.ReactNode {
+  return (
+    <div className='ez-w-1/2 ez-h-1/2 ez-m-auto'>
+      <Editor />
+    </div>
+  )
+}
+
+export default App
